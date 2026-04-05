669d8e7c25c6a3ea0fcc157176e750326ca28c97
paulkorir <paul@sci2pro.com>
2026-04-05 16:23:53 +0300

    Add Python 3 CI and publishing workflows

--- setup.py
+++ setup.py
@@ -1,32 +1,7 @@
 # -*- coding: utf-8 -*-
-# setup
+"""Compatibility shim for setuptools-based builds."""
 
-__author__ = "Paul K. Korir, PhD"
-__email__ = "paul.korir@gmail.com"
-__date__ = "2017-06-02"
+from setuptools import setup
 
-import os
 
-from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst')) as f:
-    long_description = f.read()
-
-setup(
-    name="python-units",
-    packages=find_packages(),
-    version="0.1.3",
-    author="Paul K. Korir, PhD",
-    author_email="paul.korir@gmail.com",
-    url="https://bacculus.dev/paulkorir/python-units",
-    description="Python library to represent numbers with units",
-    long_description=long_description,
-    long_description_content_type='text/x-rst; charset=UTF-8',
-    license="GNU GPL v3.0",
-    keywords="units",
-    classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3"
-    ]
-)
+setup()
